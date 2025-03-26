1afafcd5b72e406d05339c8774f6d87c5d015c73
Elineas07 <162462679+Elineas07@users.noreply.github.com>
2025-03-26 13:07:03 +0100

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,9 @@
+function finnPassord(){
+    let Passord1 = prompt("Skriv passordet")
+    let passord = "detteerpassordet"
+    if (Passord1 === passord){
+    }else{
+        location.replace("index2.html")
+    }
+}
+finnPassord()
